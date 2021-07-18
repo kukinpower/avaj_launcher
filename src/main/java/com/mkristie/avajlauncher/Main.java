57c72954f53d05d3fb3bc7a893277edb07d36b7b
kukinpower <thekukinpower@gmail.com>
2021-07-18 21:44:58 +0300

    init repo

---
+++ src/main/java/com/mkristie/avajlauncher/Main.java
@@ -0,0 +1,10 @@
+package com.mkristie.avajlauncher;
+
+public class Main {
+
+  public static void main(String[] args) {
+    if (args.length != 2 || !args[0].endsWith(".txt")) {
+      System.err.println("Please provide one and only one argument. In *.txt format");
+    }
+  }
+}
