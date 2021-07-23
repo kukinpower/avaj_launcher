16c46a7771fba26144dd74a3ddb662390dfa8a1c
kukinpower <thekukinpower@gmail.com>
2021-07-23 20:26:50 +0300

    refactor, handle err arg

--- src/main/java/com/mkristie/avajlauncher/Main.java
+++ src/main/java/com/mkristie/avajlauncher/Main.java
@@ -1,13 +1,13 @@
 package com.mkristie.avajlauncher;
 
 import com.mkristie.avajlauncher.parser.AircraftFileParser;
-import java.io.FileNotFoundException;
 
 public class Main {
 
-  public static void main(String[] args) throws FileNotFoundException {
+  public static void main(String[] args) {
     if (args.length != 1 || !args[0].endsWith(".txt")) {
       System.err.println("Please provide one and only one argument. In *.txt format");
+      return;
     }
 
     try {
